0e6cfdb53a42367c2fb61a11f7eed7d7d3b80283
xyzjesper <dm@xyzjesper>
2026-02-26 15:23:07 +0100

    Init dataSpace

--- types/YoutubeVideo.ts
+++
@@ -1,9 +0,0 @@
-export type YoutubeVideo = {
-    channelId: string;
-    channelTitle: string;
-    thumbnail?: string;
-    videoId: string;
-    title: string;
-    published: string;
-    link: string;
-}
